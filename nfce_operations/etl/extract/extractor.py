41ef93f16b0010dbd5183dff17363f361a27a057
heitor.galindo <heitor.galindo@supersim.com.br>
2022-08-07 19:50:16 -0300

    handling exception

--- nfce_operations/etl/extract/extractor.py
+++ nfce_operations/etl/extract/extractor.py
@@ -1,14 +1,17 @@
 """Extract product and NFCE data from QRCODE URL"""
 
 import requests
+from requests import RequestException
 from bs4 import BeautifulSoup
-
 
 def html_parser(url):
     """Access NFCE QRCODE URL and return html page"""
     headers = {
         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:12.0) Gecko/20100101 Firefox/12.0"}
-    print()
-    http_request = requests.get(url[0][0], headers=headers)
+    try:
+        http_request = requests.get(url, headers=headers)
+    except RequestException as error:
+        print(f"Request Error:\n{error}")
+
     parsed_hmtl = BeautifulSoup((http_request.text), "html.parser")
     return parsed_hmtl
--- nfce_operations/etl/load/loader.py
+++ nfce_operations/etl/load/loader.py
@@ -5,4 +5,7 @@ from nfce_operations.database_operations.execute_insert import execute_insert
 
 def load_to_db(query, data_list):
     """Load data to product and nfce table"""
-    execute_insert(query, data_list)
+    result = execute_insert(query, data_list)
+    if result is True:
+        return True
+    return False
